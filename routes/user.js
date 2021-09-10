fc369c1da56a2e6ef9aa15ea38127b85154d9409
yaelle <yaelle.m93@gmail.com>
2021-09-10 15:25:49 +0200

    add 2 routes

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,7 @@ const mysql = require('mysql');
 const db = require('../db');
 
 userRoute.post('/signin', (req, res) => {});
+userRoute.post('/login', (req, res) => {});
+userRoute.get('/me', (req, res) => {});
 
 module.exports = userRoute;
