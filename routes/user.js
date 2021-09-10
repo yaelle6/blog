30272af5c7109a3ea26feba7237e8744599c55e7
yaelle <yaelle.m93@gmail.com>
2021-09-10 15:27:36 +0200

    add update user route

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const db = require('../db');
 userRoute.post('/signin', (req, res) => {});
 userRoute.post('/login', (req, res) => {});
 userRoute.get('/me', (req, res) => {});
+userRoute.patch('/me', (req, res) => {});
 
 module.exports = userRoute;
